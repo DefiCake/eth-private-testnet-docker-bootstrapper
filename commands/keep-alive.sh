5a3ebe907b97f55e6963a9cf29c22e8980859199
Rafael <rafael@geodb.com>
2019-12-11 07:43:54 +0100

    Signed-off-by: Rafael <rafael@geodb.com>
    Added echo to keep alive

--- commands/keep-alive.sh
+++ commands/keep-alive.sh
@@ -6,6 +6,7 @@ while true; do
     temp="${enode%\"}"
     enode="${temp#\"}"
     if [ "$?" -eq "0" ]; then
+        echo "Running keep alive script"
         geth attach /datadir/geth.ipc --exec \
             "if(admin.peers.length===0){admin.addPeer(\"${enode}\")}" &> /dev/null
     fi
