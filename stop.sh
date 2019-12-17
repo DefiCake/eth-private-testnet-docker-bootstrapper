8a723b471204774d151069f1375d7af9d25f7c49
Rafael <rafael@geodb.com>
2019-12-17 12:32:45 +0100

    Signed-off-by: Rafael <rafael@geodb.com>
    Corrected typo

--- stop.sh
+++ stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker exec -i poa-member-bode sh -c 'kill -INT `pidof geth`'
+docker exec -i poa-member-node sh -c 'kill -INT `pidof geth`'
 docker exec -i poa-master-node sh -c 'kill -INT `pidof geth`'
 docker-compose down
